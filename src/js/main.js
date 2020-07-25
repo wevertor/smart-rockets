ce555bb9d00777f51082e63628b9fd5e834accb8
Weverton Trindade <weverton2trindade@gmail.com>
2020-07-25 11:43:18 -0300

    contador na tela

--- src/js/main.js
+++ src/js/main.js
@@ -1,25 +1,29 @@
 const populationSize = 25;
-const lifespam = 500;
+const lifespan = 500;
 
 let target;
 let population;
 let obstacles;
 let count;
 
+let lifeP;
+
 function setup() {
   createCanvas(700, 500);
 
   count = 0;
   target = new Target(width / 2, 80, 32);
-  population = new Population(populationSize, lifespam, target);
+  population = new Population(populationSize, lifespan, target);
   obstacles = [];
+
+  lifeP = createP().html(`Tempo restante: ${lifespan}`);
 }
 
 function draw() {
   background(0);
   count++;
-
-  if (count === lifespam) {
+  lifeP.html(`Tempo restante: ${lifespan - count}`);
+  if (count === lifespan) {
     population.evaluate();
     population.selection();
 
@@ -35,5 +39,26 @@ function draw() {
 }
 
 function mouseClicked() {
-  obstacles.push(new Obstacle(mouseX, mouseY, 40, 20));
+  let destroyClick = false;
+
+  const checkClick = (obstacle) => {
+    if (
+      mouseX > obstacle.x - obstacle.w / 2 &&
+      mouseX < obstacle.x + obstacle.w / 2 &&
+      mouseY > obstacle.y - obstacle.h / 2 &&
+      mouseY < obstacle.y + obstacle.h / 2
+    ) {
+      return true;
+    }
+    return false;
+  };
+
+  obstacles.forEach((obstacle, i) => {
+    if (checkClick(obstacle)) {
+      obstacles.splice(i, 1);
+      destroyClick = true;
+    }
+  });
+
+  if (!destroyClick) obstacles.push(new Obstacle(mouseX, mouseY, 40, 20));
 }
